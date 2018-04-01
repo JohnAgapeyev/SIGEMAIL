a651490d80e547eb92946a512b5233f0a5502b37
John Agapeyev <jagapeyev@gmail.com>
2018-03-31 20:13:04 -0700

    Added helpers

--- signature.c
+++ signature.c
@@ -6,6 +6,17 @@
 #include <openssl/bn.h>
 #include "signature.h"
 
+#define CURVE_25519_N 2
+#define CURVE_25519_C 8
+#define CURVE_25519_A 486662
+#define CURVE_25519_B 256
+#define CURVE_25519_ORDER_P 255
+#define CURVE_25519_ORDER_Q 253
+
+static BIGNUM * get_curve25519_p(void);
+static BIGNUM * get_curve25519_q(void);
+static BIGNUM * get_curve25519_d(void);
+
 struct edwards_key_pair {
     BIGNUM *private;
     BIGNUM *public;
@@ -35,14 +46,57 @@ unsigned char * vxeddsa_verify(unsigned char mont_pub[crypto_kx_PUBLICKEYBYTES],
 }
 
 BIGNUM * convert_mont(BIGNUM *u) {
-    return NULL;
+    BN_CTX *ctx = BN_CTX_new();
+
+    //power == 2^255
+    BIGNUM *power = BN_new();
+    BN_one(power);
+    BN_lshift(power, power, 255);
+
+    //u_masked = u mod 2^255
+    BIGNUM *u_masked = BN_dup(u);
+    BN_nnmod(u_masked, u_masked, power, ctx);
+
+    BIGNUM *converted = u_to_y(u_masked);
+
+    BN_clear_bit(converted, 255);
+
+    BN_CTX_free(ctx);
+    BN_free(u_masked);
+    BN_free(power);
+
+    return converted;
 }
 
 BIGNUM * u_to_y(BIGNUM *u) {
-    return NULL;
+    BIGNUM *p = get_curve25519_p();
+
+    BIGNUM *minus = BN_dup(u);
+    BIGNUM *inverse = BN_dup(u);
+
+    BN_sub(minus, minus, BN_value_one());
+
+    BN_add(inverse, inverse, BN_value_one());
+
+    BN_CTX *ctx = BN_CTX_new();
+
+    BN_mod_inverse(inverse, inverse, p, ctx);
+
+    BN_mod_mul(inverse, minus, inverse, p, ctx);
+
+    BN_free(p);
+    BN_free(minus);
+    BN_CTX_free(ctx);
+
+    return inverse;
 }
 
 struct edwards_key_pair * calculate_key_pair(BIGNUM *u) {
+    unsigned char multiple[crypto_scalarmult_ed25519_BYTES];
+
+
+
+
     return NULL;
 }
 
@@ -57,3 +111,57 @@ BIGNUM * elligator2(BIGNUM *r) {
 BIGNUM * hash_to_point(unsigned char *mesg, size_t len) {
     return NULL;
 }
+
+BIGNUM * get_curve25519_p(void) {
+    BIGNUM *nineteen = BN_new();
+    BN_set_word(nineteen, 19);
+
+    BIGNUM *power = BN_new();
+    BN_one(power);
+    BN_lshift(power, power, 255);
+
+    BN_sub(power, power, nineteen);
+
+    BN_free(nineteen);
+
+    return power;
+}
+
+BIGNUM * get_curve25519_q(void) {
+    BIGNUM *power = BN_new();
+    BN_one(power);
+    BN_lshift(power, power, 252);
+
+    BIGNUM *add = NULL;
+
+    BN_dec2bn(&add, "27742317777372353535851937790883648493");
+
+    BN_add(power, power, add);
+
+    BN_free(add);
+
+    return power;
+}
+
+BIGNUM * get_curve25519_d(void) {
+    BIGNUM *p = get_curve25519_p();
+
+    BIGNUM *num = BN_new();
+    BIGNUM *denom = BN_new();
+
+    BN_set_word(num, -121665);
+    BN_set_word(denom, 121666);
+
+    BN_CTX *ctx = BN_CTX_new();
+
+    BN_div(num, NULL, num, denom, ctx);
+
+    BN_nnmod(num, num, p, ctx);
+
+    BN_free(p);
+    BN_free(denom);
+    BN_CTX_free(ctx);
+
+    return num;
+}
+
