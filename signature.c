d0c0b7fbb99ed7698e1929adb46514dee972a531
John Agapeyev <jagapeyev@gmail.com>
2018-03-31 19:29:45 -0700

    Added helper function prototypes

--- signature.c
+++ signature.c
@@ -6,6 +6,18 @@
 #include <openssl/bn.h>
 #include "signature.h"
 
+struct edwards_key_pair {
+    BIGNUM *private;
+    BIGNUM *public;
+};
+
+static BIGNUM * convert_mont(BIGNUM *u);
+static BIGNUM * u_to_y(BIGNUM *u);
+static struct edwards_key_pair * calculate_key_pair(BIGNUM *u);
+static unsigned char * hash_i(int i, unsigned char *mesg, size_t len);
+static BIGNUM * elligator2(BIGNUM *r);
+static BIGNUM * hash_to_point(unsigned char *mesg, size_t len);
+
 struct xeddsa_signature * xeddsa_sign(unsigned char mont_priv[crypto_kx_SECRETKEYBYTES], unsigned char *mesg, size_t len, unsigned char rand_data[64]) {
     return NULL;
 }
@@ -22,3 +34,26 @@ unsigned char * vxeddsa_verify(unsigned char mont_pub[crypto_kx_PUBLICKEYBYTES],
     return NULL;
 }
 
+BIGNUM * convert_mont(BIGNUM *u) {
+    return NULL;
+}
+
+BIGNUM * u_to_y(BIGNUM *u) {
+    return NULL;
+}
+
+struct edwards_key_pair * calculate_key_pair(BIGNUM *u) {
+    return NULL;
+}
+
+unsigned char * hash_i(int i, unsigned char *mesg, size_t len) {
+    return NULL;
+}
+
+BIGNUM * elligator2(BIGNUM *r) {
+    return NULL;
+}
+
+BIGNUM * hash_to_point(unsigned char *mesg, size_t len) {
+    return NULL;
+}
