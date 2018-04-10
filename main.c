2bc71a3ee42860bd6c100a32a92c3a4ec8db747c
John Agapeyev <jagapeyev@gmail.com>
2018-04-10 12:13:17 -0700

    Added initial Cmake build file

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "test.h"
 
 int main(void) {
+    testing();
     return EXIT_SUCCESS;
 }
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test.h"
+
+void testing(void) {
+    printf("This is a thing that has run\n");
+}
