f78c8482012fb204577a9d7489426dfe864343ad
John Agapeyev <jagapeyev@gmail.com>
2018-06-10 16:28:03 -0700

    Converted kdf to C++ idioms

--- src/kdf.cpp
+++ src/kdf.cpp
@@ -1,23 +1,23 @@
-#include <stdint.h>
-#include <string.h>
+#include <cstdint>
+#include <cstring>
 #include <openssl/evp.h>
 #include "kdf.h"
 
 #define KDF_COUNT 10000
 
-void root_derive(uint8_t root_key[], uint8_t dh_output[], uint8_t out_root[], uint8_t out_chain[]) {
+void root_derive(std::array<std::byte, 32> root_key, std::array<std::byte, 32> dh_output, std::array<std::byte, 32> out_root, std::array<std::byte, 32> out_chain) {
     uint8_t temp[64];
-    PKCS5_PBKDF2_HMAC((const char *const) dh_output, sizeof(uint8_t) * 32, root_key, sizeof(uint8_t) * 32, KDF_COUNT, EVP_sha512(), 64, temp);
+    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(dh_output.data()), sizeof(uint8_t) * 32, reinterpret_cast<unsigned char *>(root_key.data()), sizeof(uint8_t) * 32, KDF_COUNT, EVP_sha512(), 64, temp);
 
     //Write key halves into out parameters
-    memcpy(out_root, temp, 32);
-    memcpy(out_chain, temp + 32, 32);
+    memcpy(out_root.data(), temp, 32);
+    memcpy(out_chain.data(), temp + 32, 32);
 }
 
-void chain_derive(uint8_t chain_key[], uint8_t out_chain[], uint8_t out_message[]) {
+void chain_derive(std::array<std::byte, 32> chain_key, std::array<std::byte, 32> out_chain, std::array<std::byte, 32> out_message) {
     const unsigned char in_1 = 0x01;
     const unsigned char in_2 = 0x01;
-    PKCS5_PBKDF2_HMAC((const char *const) chain_key, sizeof(uint8_t) * 32, &in_1, 1, KDF_COUNT, EVP_sha512(), 32, out_chain);
-    PKCS5_PBKDF2_HMAC((const char *const) chain_key, sizeof(uint8_t) * 32, &in_2, 1, KDF_COUNT, EVP_sha512(), 32, out_message);
+    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(chain_key.data()), sizeof(uint8_t) * 32, &in_1, 1, KDF_COUNT, EVP_sha512(), 32, reinterpret_cast<unsigned char *>(out_chain.data()));
+    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(chain_key.data()), sizeof(uint8_t) * 32, &in_2, 1, KDF_COUNT, EVP_sha512(), 32, reinterpret_cast<unsigned char *>(out_message.data()));
 }
 
--- src/kdf.h
+++ src/kdf.h
@@ -1,9 +1,11 @@
 #ifndef KDF_H
 #define KDF_H
 
-#include <stdint.h>
+#include <cstdint>
+#include <array>
+#include <cstddef>
 
-void root_derive(uint8_t root_key[], uint8_t dh_output[], uint8_t out_root[], uint8_t out_chain[]);
-void chain_derive(uint8_t chain_key[], uint8_t out_chain[], uint8_t out_message[]);
+void root_derive(std::array<std::byte, 32> root_key, std::array<std::byte, 32> dh_output, std::array<std::byte, 32> out_root, std::array<std::byte, 32> out_chain);
+void chain_derive(std::array<std::byte, 32> chain_key, std::array<std::byte, 32> out_chain, std::array<std::byte, 32> out_message);
 
 #endif
