5208c51704505a99957d79e785b1c9a98ded9870
John Agapeyev <jagapeyev@gmail.com>
2018-06-10 17:21:45 -0700

    Finally finished cleaning up the kdf functions

--- src/crypto.h
+++ src/crypto.h
@@ -13,8 +13,8 @@ namespace crypto {
     void encrypt(const std::vector<std::byte>& message, const std::array<std::byte, 32>& key, const std::vector<std::byte>& aad, std::vector<std::byte>& ciphertext, std::array<std::byte, 16>& tag);
     bool decrypt(const std::vector<std::byte>& ciphertext, std::array<std::byte, 16>& tag, const std::array<std::byte, 32>& key, const std::vector<std::byte>& aad, std::vector<std::byte>& plaintext);
 
-    void root_derive(std::array<std::byte, 32> root_key, std::array<std::byte, 32> dh_output, std::array<std::byte, 32> out_root, std::array<std::byte, 32> out_chain);
-    void chain_derive(std::array<std::byte, 32> chain_key, std::array<std::byte, 32> out_chain, std::array<std::byte, 32> out_message);
+    std::array<std::byte, 32> root_derive(std::array<std::byte, 32>& root_key, const std::array<std::byte, 32>& dh_output);
+    std::array<std::byte, 32> chain_derive(std::array<std::byte, 32>& chain_key);
 }
 
 #endif
--- src/kdf.cpp
+++ src/kdf.cpp
@@ -5,19 +5,27 @@
 
 #define KDF_COUNT 10000
 
-void crypto::root_derive(std::array<std::byte, 32> root_key, std::array<std::byte, 32> dh_output, std::array<std::byte, 32> out_root, std::array<std::byte, 32> out_chain) {
-    uint8_t temp[64];
-    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(dh_output.data()), sizeof(uint8_t) * 32, reinterpret_cast<unsigned char *>(root_key.data()), sizeof(uint8_t) * 32, KDF_COUNT, EVP_sha512(), 64, temp);
+std::array<std::byte, 32> crypto::root_derive(std::array<std::byte, 32>& root_key, const std::array<std::byte, 32>& dh_output) {
+    std::array<std::byte, 64> temp;
+    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(dh_output.data()), sizeof(std::byte) * 32, reinterpret_cast<unsigned char *>(root_key.data()), sizeof(std::byte) * 32, KDF_COUNT, EVP_sha512(), sizeof(std::byte) * 64, reinterpret_cast<unsigned char *>(temp.data()));
+
+    std::array<std::byte, 32> chain_key;
 
     //Write key halves into out parameters
-    memcpy(out_root.data(), temp, 32);
-    memcpy(out_chain.data(), temp + 32, 32);
+    memcpy(root_key.data(), temp.data(), 32);
+    memcpy(chain_key.data(), temp.data() + 32, 32);
+
+    return chain_key;
 }
 
-void crypto::chain_derive(std::array<std::byte, 32> chain_key, std::array<std::byte, 32> out_chain, std::array<std::byte, 32> out_message) {
+std::array<std::byte, 32> crypto::chain_derive(std::array<std::byte, 32>& chain_key) {
     const unsigned char in_1 = 0x01;
     const unsigned char in_2 = 0x01;
-    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(chain_key.data()), sizeof(uint8_t) * 32, &in_1, 1, KDF_COUNT, EVP_sha512(), 32, reinterpret_cast<unsigned char *>(out_chain.data()));
-    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(chain_key.data()), sizeof(uint8_t) * 32, &in_2, 1, KDF_COUNT, EVP_sha512(), 32, reinterpret_cast<unsigned char *>(out_message.data()));
+    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(chain_key.data()), sizeof(std::byte) * 32, &in_1, 1, KDF_COUNT, EVP_sha512(), sizeof(std::byte) * 32, reinterpret_cast<unsigned char *>(chain_key.data()));
+
+    std::array<std::byte, 32> message_key;
+    PKCS5_PBKDF2_HMAC(reinterpret_cast<const char *>(chain_key.data()), sizeof(std::byte) * 32, &in_2, 1, KDF_COUNT, EVP_sha512(), sizeof(std::byte) * 32, reinterpret_cast<unsigned char *>(message_key.data()));
+
+    return message_key;
 }
 
