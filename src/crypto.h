f3fa29dae4da290be1fc4a1133814c24184211a9
John Agapeyev <jagapeyev@gmail.com>
2018-06-10 17:05:43 -0700

    Converted existing C code to C++ conatiners

--- src/crypto.h
+++ src/crypto.h
@@ -5,12 +5,13 @@
 #include <cstdlib>
 #include <array>
 #include <cstddef>
+#include <vector>
 
 namespace crypto {
     class DH_Keypair;
 
-    void encrypt(const uint8_t *const message, const size_t mesg_len, const uint8_t key[], const uint8_t *const aad, const size_t aad_len, uint8_t *const ciphertext, uint8_t tag[]);
-    bool decrypt(const uint8_t *const ciphertext, const size_t cipher_len, const uint8_t tag[], const uint8_t key[], const uint8_t *const aad, const size_t aad_len, uint8_t *const plaintext);
+    void encrypt(const std::vector<std::byte>& message, const std::array<std::byte, 32>& key, const std::vector<std::byte>& aad, std::vector<std::byte>& ciphertext, std::array<std::byte, 16>& tag);
+    bool decrypt(const std::vector<std::byte>& ciphertext, std::array<std::byte, 16>& tag, const std::array<std::byte, 32>& key, const std::vector<std::byte>& aad, std::vector<std::byte>& plaintext);
 
     void root_derive(std::array<std::byte, 32> root_key, std::array<std::byte, 32> dh_output, std::array<std::byte, 32> out_root, std::array<std::byte, 32> out_chain);
     void chain_derive(std::array<std::byte, 32> chain_key, std::array<std::byte, 32> out_chain, std::array<std::byte, 32> out_message);
--- src/encrypt.cpp
+++ src/encrypt.cpp
@@ -1,9 +1,10 @@
 #include <cstdint>
 #include <cstdlib>
+#include <vector>
 #include <openssl/evp.h>
 #include "crypto.h"
 
-void crypto::encrypt(const uint8_t *const  message, const size_t mesg_len, const uint8_t key[], const uint8_t *const  aad, const size_t aad_len, uint8_t *const  ciphertext, uint8_t tag[]) {
+void crypto::encrypt(const std::vector<std::byte>& message, const std::array<std::byte, 32>& key, const std::vector<std::byte>& aad, std::vector<std::byte>& ciphertext, std::array<std::byte, 16>& tag) {
     //THIS IS NOT A PROBLEM
     //Message keys are only used once in Signal, so nonce reuse is not an issue
     //See https://signal.org/docs/specifications/doubleratchet/#external-functions under the ENCRYPT function for confirmation
@@ -13,21 +14,21 @@ void crypto::encrypt(const uint8_t *const  message, const size_t mesg_len, const
     }
 
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    EVP_EncryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL, key, nonce);
+    EVP_EncryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL, reinterpret_cast<const unsigned char *>(key.data()), nonce);
 
     int len;
-    EVP_EncryptUpdate(ctx, NULL, &len, aad, aad_len);
+    EVP_EncryptUpdate(ctx, NULL, &len, reinterpret_cast<const unsigned char *>(aad.data()), aad.size());
 
-    EVP_EncryptUpdate(ctx, ciphertext, &len, message, mesg_len);
+    EVP_EncryptUpdate(ctx, reinterpret_cast<unsigned char *>(ciphertext.data()), &len, reinterpret_cast<const unsigned char *>(message.data()), message.size());
 
-    EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
+    EVP_EncryptFinal_ex(ctx, reinterpret_cast<unsigned char *>(ciphertext.data()) + len, &len);
 
-    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag);
+    EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, reinterpret_cast<unsigned char *>(tag.data()));
 
     EVP_CIPHER_CTX_free(ctx);
 }
 
-bool crypto::decrypt(const uint8_t *const  ciphertext, const size_t cipher_len, const uint8_t tag[], const uint8_t key[], const uint8_t *const  aad, const size_t aad_len, uint8_t *const  plaintext) {
+bool crypto::decrypt(const std::vector<std::byte>& ciphertext, std::array<std::byte, 16>& tag, const std::array<std::byte, 32>& key, const std::vector<std::byte>& aad, std::vector<std::byte>& plaintext) {
     //THIS IS NOT A PROBLEM
     //Message keys are only used once in Signal, so nonce reuse is not an issue
     //See https://signal.org/docs/specifications/doubleratchet/#external-functions under the ENCRYPT function for confirmation
@@ -38,19 +39,19 @@ bool crypto::decrypt(const uint8_t *const  ciphertext, const size_t cipher_len, 
 
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
 
-    EVP_DecryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL, key, nonce);
+    EVP_DecryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL, reinterpret_cast<const unsigned char *>(key.data()), nonce);
 
     int len;
-    EVP_DecryptUpdate(ctx, NULL, &len, aad, aad_len);
+    EVP_DecryptUpdate(ctx, NULL, &len, reinterpret_cast<const unsigned char *>(aad.data()), aad.size());
 
-    EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, cipher_len);
+    EVP_DecryptUpdate(ctx, reinterpret_cast<unsigned char *>(plaintext.data()), &len, reinterpret_cast<const unsigned char *>(ciphertext.data()), ciphertext.size());
 
-    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, (uint8_t *) tag)) {
+    if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16, reinterpret_cast<unsigned char *>(tag.data()))) {
         //TEMP
         abort();
     }
 
-    int ret = EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+    int ret = EVP_DecryptFinal_ex(ctx, reinterpret_cast<unsigned char *>(plaintext.data()) + len, &len);
 
     EVP_CIPHER_CTX_free(ctx);
 
