70bd98bf8c57fa5be59236211cc6b9df794cafd0
John Agapeyev <jagapeyev@gmail.com>
2018-07-22 11:30:23 -0700

    Made zallocator inherit from allocator_traits

--- src/crypto.h
+++ src/crypto.h
@@ -23,7 +23,7 @@ namespace crypto {
 
     //Taken and modified from https://wiki.openssl.org/index.php/EVP_Symmetric_Encryption_and_Decryption#C.2B.2B_Programs
     template <typename T>
-    struct zallocator {
+    struct zallocator : public std::allocator_traits<zallocator<T>> {
         using value_type = T;
         using pointer = value_type *;
         using const_pointer = const value_type *;
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,14 @@
 #include <algorithm>
 #include "crypto.h"
 
-int main(void) {
+void test() {
     crypto::secure_array<int, 32> good{1, 4, 3, 5, 7, 8, 9, 2};
     std::cout << good[3] << "\n";
     good.fill(99);
     std::cout << good[3] << "\n";
+}
+
+int main(void) {
+    test();
     return EXIT_SUCCESS;
 }
