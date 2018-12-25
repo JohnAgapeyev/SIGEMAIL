16d1f81ebad5c23cdb827dfc12b900e93abd93e2
John Agapeyev <jagapeyev@gmail.com>
2018-12-25 14:07:25 -0800

    Substituted more alias types

--- src/key_pack.h
+++ src/key_pack.h
@@ -7,7 +7,7 @@
 class initial_key_pack {
 public:
     initial_key_pack(const crypto::DH_Keypair& signing_keys,
-            const crypto::secure_array<std::byte, 32>& pre_key,
+            const crypto::public_key& pre_key,
             const crypto::secure_vector<crypto::DH_Keypair>& one_time_keys);
     ~initial_key_pack() = default;
     initial_key_pack(const initial_key_pack&) = default;
@@ -16,16 +16,16 @@ public:
     initial_key_pack& operator=(const initial_key_pack&) = default;
 
 private:
-    crypto::secure_array<std::byte, 32> identity_public_key;
-    crypto::secure_array<std::byte, 32> signed_pre_key;
-    crypto::secure_array<std::byte, 64> pre_key_signature;
+    crypto::public_key identity_public_key;
+    crypto::public_key signed_pre_key;
+    crypto::signature pre_key_signature;
     crypto::secure_vector<crypto::DH_Keypair> one_time_pre_keys;
 };
 
 class pre_key_update {
 public:
     pre_key_update(const crypto::DH_Keypair& signing_keys,
-            const crypto::secure_array<std::byte, 32>& pre_key);
+            const crypto::public_key& pre_key);
     ~pre_key_update() = default;
     pre_key_update(const pre_key_update&) = default;
     pre_key_update(pre_key_update&&) = default;
@@ -33,8 +33,8 @@ public:
     pre_key_update& operator=(const pre_key_update&) = default;
 
 private:
-    crypto::secure_array<std::byte, 32> signed_pre_key;
-    crypto::secure_array<std::byte, 64> pre_key_signature;
+    crypto::public_key signed_pre_key;
+    crypto::signature pre_key_signature;
 };
 
 #endif /* end of include guard: KEY_PACK_H */
--- src/message.h
+++ src/message.h
@@ -8,7 +8,7 @@
 
 struct signal_message {
     struct header {
-        crypto::secure_array<std::byte, 32> dh_public_key;
+        crypto::public_key dh_public_key;
         uint64_t prev_chain_len;
         uint64_t message_num;
     } header;
@@ -18,10 +18,10 @@ struct signal_message {
 
 struct initial_signal_message {
     struct header {
-        crypto::secure_array<std::byte, 32> identity_key;
-        crypto::secure_array<std::byte, 32> ephemeral_key;
+        crypto::public_key identity_key;
+        crypto::public_key ephemeral_key;
         //This is the public key of the one-time key that was used in the initial message
-        std::optional<crypto::secure_array<std::byte, 32>> remote_one_time_public_key;
+        std::optional<crypto::public_key> remote_one_time_public_key;
     } header;
     crypto::secure_vector<std::byte> message;
     crypto::secure_vector<std::byte> aad;
