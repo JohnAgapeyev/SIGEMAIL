1a724ef1d7476499925f1505cea35b4f6d04ea24
John Agapeyev <jagapeyev@gmail.com>
2018-12-25 13:58:43 -0800

    Made encrypt code exception safe, added some openssl error checking, and added a couple new tests

--- src/encrypt.cpp
+++ src/encrypt.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <cstdlib>
+#include <memory>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 #include <vector>
@@ -11,28 +12,42 @@ const crypto::secure_vector<std::byte> crypto::encrypt(const secure_vector<std::
     std::array<std::byte, 12> nonce;
     RAND_bytes(reinterpret_cast<unsigned char*>(nonce.data()), 12);
 
-    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-    EVP_EncryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL,
-            reinterpret_cast<const unsigned char*>(key.data()),
-            reinterpret_cast<const unsigned char*>(nonce.data()));
+    std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_free)> ctx{
+            EVP_CIPHER_CTX_new(), &EVP_CIPHER_CTX_free};
+    if (!ctx) {
+        throw std::bad_alloc();
+    }
+
+    if (!EVP_EncryptInit_ex(ctx.get(), EVP_chacha20_poly1305(), NULL,
+                reinterpret_cast<const unsigned char*>(key.data()),
+                reinterpret_cast<const unsigned char*>(nonce.data()))) {
+        throw std::runtime_error("EVP_EncryptInit_ex");
+    }
 
     int len;
-    EVP_EncryptUpdate(
-            ctx, NULL, &len, reinterpret_cast<const unsigned char*>(aad.data()), aad.size());
+    if (!EVP_EncryptUpdate(ctx.get(), NULL, &len,
+                reinterpret_cast<const unsigned char*>(aad.data()), aad.size())) {
+        throw std::domain_error("EVP_EncryptUpdate");
+    }
 
     secure_vector<std::byte> ciphertext;
     ciphertext.resize(message.size());
 
-    EVP_EncryptUpdate(ctx, reinterpret_cast<unsigned char*>(ciphertext.data()), &len,
-            reinterpret_cast<const unsigned char*>(message.data()), message.size());
+    if (!EVP_EncryptUpdate(ctx.get(), reinterpret_cast<unsigned char*>(ciphertext.data()), &len,
+                reinterpret_cast<const unsigned char*>(message.data()), message.size())) {
+        throw std::domain_error("EVP_EncryptUpdate");
+    }
 
-    EVP_EncryptFinal_ex(ctx, reinterpret_cast<unsigned char*>(ciphertext.data()) + len, &len);
+    if (!EVP_EncryptFinal_ex(
+                ctx.get(), reinterpret_cast<unsigned char*>(ciphertext.data()) + len, &len)) {
+        throw std::domain_error("EVP_EncryptFinal_ex");
+    }
 
     secure_vector<std::byte> tag;
     tag.resize(16);
 
     EVP_CIPHER_CTX_ctrl(
-            ctx, EVP_CTRL_GCM_GET_TAG, 16, reinterpret_cast<unsigned char*>(tag.data()));
+            ctx.get(), EVP_CTRL_GCM_GET_TAG, 16, reinterpret_cast<unsigned char*>(tag.data()));
 
     //Insert the tag at the end of the ciphertext
     ciphertext.insert(ciphertext.end(), tag.begin(), tag.end());
@@ -40,15 +55,13 @@ const crypto::secure_vector<std::byte> crypto::encrypt(const secure_vector<std::
     //Insert nonce at the beginning
     ciphertext.insert(ciphertext.begin(), nonce.begin(), nonce.end());
 
-    EVP_CIPHER_CTX_free(ctx);
-
     return ciphertext;
 }
 
 const crypto::secure_vector<std::byte> crypto::decrypt(secure_vector<std::byte>& ciphertext,
         const crypto::shared_key& key, const secure_vector<std::byte>& aad) {
     if (ciphertext.size() <= 28) {
-        throw std::runtime_error("Received message was too short to decrypt");
+        throw std::invalid_argument("Received message was too short to decrypt");
     }
 
     std::array<std::byte, 12> nonce;
@@ -56,39 +69,42 @@ const crypto::secure_vector<std::byte> crypto::decrypt(secure_vector<std::byte>&
         nonce[i] = ciphertext[i];
     }
 
-    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-
-    try {
-        EVP_DecryptInit_ex(ctx, EVP_chacha20_poly1305(), NULL,
-                reinterpret_cast<const unsigned char*>(key.data()),
-                reinterpret_cast<const unsigned char*>(nonce.data()));
-
-        int len;
-        EVP_DecryptUpdate(
-                ctx, NULL, &len, reinterpret_cast<const unsigned char*>(aad.data()), aad.size());
-
-        secure_vector<std::byte> plaintext;
-        plaintext.resize(ciphertext.size() - 28);
-
-        EVP_DecryptUpdate(ctx, reinterpret_cast<unsigned char*>(plaintext.data()), &len,
-                reinterpret_cast<const unsigned char*>(ciphertext.data() + 12),
-                ciphertext.size() - 28);
-
-        if (!EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, 16,
-                    reinterpret_cast<unsigned char*>(ciphertext.data() + ciphertext.size() - 16))) {
-            throw std::runtime_error("Message tag failed to enter correctly");
-        }
-
-        if (!EVP_DecryptFinal_ex(
-                    ctx, reinterpret_cast<unsigned char*>(plaintext.data()) + len, &len)) {
-            throw std::runtime_error("Message failed to decrypt");
-        }
-
-        EVP_CIPHER_CTX_free(ctx);
-
-        return plaintext;
-    } catch (...) {
-        EVP_CIPHER_CTX_free(ctx);
-        throw;
+    std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_free)> ctx{
+            EVP_CIPHER_CTX_new(), &EVP_CIPHER_CTX_free};
+    if (!ctx) {
+        throw std::bad_alloc();
     }
+
+    if (!EVP_DecryptInit_ex(ctx.get(), EVP_chacha20_poly1305(), NULL,
+                reinterpret_cast<const unsigned char*>(key.data()),
+                reinterpret_cast<const unsigned char*>(nonce.data()))) {
+        throw std::domain_error("EVP_DecryptInit_ex");
+    }
+
+    int len;
+    if (!EVP_DecryptUpdate(ctx.get(), NULL, &len,
+                reinterpret_cast<const unsigned char*>(aad.data()), aad.size())) {
+        throw std::domain_error("EVP_DecryptUpdate");
+    }
+
+    secure_vector<std::byte> plaintext;
+    plaintext.resize(ciphertext.size() - 28);
+
+    if (!EVP_DecryptUpdate(ctx.get(), reinterpret_cast<unsigned char*>(plaintext.data()), &len,
+                reinterpret_cast<const unsigned char*>(ciphertext.data() + 12),
+                ciphertext.size() - 28)) {
+        throw std::domain_error("EVP_DecryptUpdate");
+    }
+
+    if (!EVP_CIPHER_CTX_ctrl(ctx.get(), EVP_CTRL_GCM_SET_TAG, 16,
+                reinterpret_cast<unsigned char*>(ciphertext.data() + ciphertext.size() - 16))) {
+        throw std::invalid_argument("Message tag failed to enter correctly");
+    }
+
+    if (!EVP_DecryptFinal_ex(
+                ctx.get(), reinterpret_cast<unsigned char*>(plaintext.data()) + len, &len)) {
+        throw std::runtime_error("Message failed to decrypt");
+    }
+
+    return plaintext;
 }
--- src/test/encrypt_test.cpp
+++ src/test/encrypt_test.cpp
@@ -11,19 +11,7 @@ BOOST_AUTO_TEST_SUITE(encryption_tests)
 BOOST_AUTO_TEST_CASE(basic_encryption) {
     const auto message = get_message();
     const auto key = get_key();
-    const auto aad = get_message();
-
-    const auto ciphertext = crypto::encrypt(message, key, aad);
-    auto ciphertext_copy = ciphertext;
-    const auto plaintext = crypto::decrypt(ciphertext_copy, key, aad);
-
-    BOOST_TEST(message == plaintext);
-}
-
-BOOST_AUTO_TEST_CASE(aad_encryption) {
-    const auto message = get_message();
-    const auto key = get_key();
-    const auto aad = get_message();
+    const auto aad = get_aad();
 
     const auto ciphertext = crypto::encrypt(message, key, aad);
     auto ciphertext_copy = ciphertext;
@@ -59,6 +47,50 @@ BOOST_AUTO_TEST_CASE(corrupted_aad) {
     aad[2] = std::byte{0x00};
     aad[8] = std::byte{0xFF};
     aad[12] = std::byte{0xCA};
+
+    crypto::secure_vector<std::byte> plaintext;
+    BOOST_REQUIRE_THROW(plaintext = crypto::decrypt(ciphertext_copy, key, aad), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(too_short_decrypt) {
+    const auto message = get_message();
+    const auto key = get_key();
+    auto aad = get_message();
+
+    const auto ciphertext = crypto::encrypt(message, key, aad);
+
+    auto ciphertext_copy = ciphertext;
+    ciphertext_copy.resize(12);
+
+    crypto::secure_vector<std::byte> plaintext;
+    BOOST_REQUIRE_THROW(plaintext = crypto::decrypt(ciphertext_copy, key, aad), std::invalid_argument);
+}
+
+BOOST_AUTO_TEST_CASE(bad_key) {
+    const auto message = get_message();
+    auto key = get_key();
+    auto aad = get_message();
+
+    const auto ciphertext = crypto::encrypt(message, key, aad);
+
+    auto ciphertext_copy = ciphertext;
+
+    key[2] ^= std::byte{0xcd};
+
+    crypto::secure_vector<std::byte> plaintext;
+    BOOST_REQUIRE_THROW(plaintext = crypto::decrypt(ciphertext_copy, key, aad), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(bad_nonce) {
+    const auto message = get_message();
+    const auto key = get_key();
+    auto aad = get_message();
+
+    const auto ciphertext = crypto::encrypt(message, key, aad);
+
+    auto ciphertext_copy = ciphertext;
+
+    ciphertext_copy[1] <<= 2;
 
     crypto::secure_vector<std::byte> plaintext;
     BOOST_REQUIRE_THROW(plaintext = crypto::decrypt(ciphertext_copy, key, aad), std::runtime_error);
