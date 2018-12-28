32292e6da4c4140bb880fb035a0533ae6976db15
John Agapeyev <jagapeyev@gmail.com>
2018-12-27 21:13:25 -0800

    Converted beast server example to files and made it compile

---
+++ src/server/server_network.cpp
@@ -0,0 +1,151 @@
+#include <algorithm>
+#include <boost/asio/bind_executor.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "server_network.h"
+
+using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
+namespace ssl = boost::asio::ssl; // from <boost/asio/ssl.hpp>
+namespace websocket = boost::beast::websocket; // from <boost/beast/websocket.hpp>
+
+// Start the asynchronous operation
+void server_network_session::run() {
+    // Perform the SSL handshake
+    ws_.next_layer().async_handshake(ssl::stream_base::server,
+            boost::asio::bind_executor(strand_,
+                    std::bind(&server_network_session::on_handshake, shared_from_this(),
+                            std::placeholders::_1)));
+}
+
+void server_network_session::on_handshake(boost::system::error_code ec) {
+    if (ec) {
+        //Handshake failed
+        return;
+    }
+
+    // Accept the websocket handshake
+    ws_.async_accept(boost::asio::bind_executor(strand_,
+            std::bind(&server_network_session::on_accept, shared_from_this(),
+                    std::placeholders::_1)));
+}
+
+void server_network_session::on_accept(boost::system::error_code ec) {
+    if (ec) {
+        //Accept failed
+        return;
+    }
+
+    // Read a message
+    do_read();
+}
+
+void server_network_session::do_read() {
+    // Read a message into our buffer
+    ws_.async_read(buffer_,
+            boost::asio::bind_executor(strand_,
+                    std::bind(&server_network_session::on_read, shared_from_this(),
+                            std::placeholders::_1, std::placeholders::_2)));
+}
+
+void server_network_session::on_read(boost::system::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    // This indicates that the server_network_session was closed
+    if (ec == websocket::error::closed)
+        return;
+
+    if (ec) {
+        //Read failed
+    }
+
+    // Echo the message
+    ws_.text(ws_.got_text());
+    ws_.async_write(buffer_.data(),
+            boost::asio::bind_executor(strand_,
+                    std::bind(&server_network_session::on_write, shared_from_this(),
+                            std::placeholders::_1, std::placeholders::_2)));
+}
+
+void server_network_session::on_write(boost::system::error_code ec, std::size_t bytes_transferred) {
+    boost::ignore_unused(bytes_transferred);
+
+    if (ec) {
+        //Write failed
+        return;
+    }
+
+    // Clear the buffer
+    buffer_.consume(buffer_.size());
+
+    // Do another read
+    do_read();
+}
+
+listener::listener(boost::asio::io_context& ioc, ssl::context& ctx, tcp::endpoint endpoint) :
+        ctx_(ctx), acceptor_(ioc), socket_(ioc) {
+    boost::system::error_code ec;
+
+    // Open the acceptor
+    acceptor_.open(endpoint.protocol(), ec);
+    if (ec) {
+        //Open failed
+        return;
+    }
+
+    // Allow address reuse
+    acceptor_.set_option(boost::asio::socket_base::reuse_address(true), ec);
+    if (ec) {
+        return;
+    }
+
+    // Bind to the server address
+    acceptor_.bind(endpoint, ec);
+    if (ec) {
+        //Bind failed
+        return;
+    }
+
+    // Start listening for connections
+    acceptor_.listen(boost::asio::socket_base::max_listen_connections, ec);
+    if (ec) {
+        //Listen failed
+        return;
+    }
+}
+
+// Start accepting incoming connections
+void listener::run() {
+    if (!acceptor_.is_open()) {
+        return;
+    }
+    do_accept();
+}
+
+void listener::do_accept() {
+    acceptor_.async_accept(
+            socket_, std::bind(&listener::on_accept, shared_from_this(), std::placeholders::_1));
+}
+
+void listener::on_accept(boost::system::error_code ec) {
+    if (ec) {
+        //Accept failed
+    } else {
+        // Create the server_network_session and run it
+        std::make_shared<server_network_session>(std::move(socket_), ctx_)->run();
+    }
+
+    // Accept another connection
+    do_accept();
+}
---
+++ src/server/server_network.h
@@ -0,0 +1,64 @@
+#ifndef SERVER_NETWORK_H
+#define SERVER_NETWORK_H
+
+#include <algorithm>
+#include <boost/asio/bind_executor.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+using tcp = boost::asio::ip::tcp; // from <boost/asio/ip/tcp.hpp>
+namespace ssl = boost::asio::ssl; // from <boost/asio/ssl.hpp>
+namespace websocket = boost::beast::websocket; // from <boost/beast/websocket.hpp>
+
+// Echoes back all received WebSocket messages
+class server_network_session : public std::enable_shared_from_this<server_network_session> {
+public:
+    // Take ownership of the socket
+    server_network_session(tcp::socket socket, ssl::context& ctx) :
+            socket_(std::move(socket)), ws_(socket_, ctx), strand_(ws_.get_executor()) {}
+
+    // Start the asynchronous operation
+    void run();
+    void on_handshake(boost::system::error_code ec);
+    void on_accept(boost::system::error_code ec);
+    void do_read();
+    void on_read(boost::system::error_code ec, std::size_t bytes_transferred);
+    void on_write(boost::system::error_code ec, std::size_t bytes_transferred);
+
+private:
+    tcp::socket socket_;
+    websocket::stream<ssl::stream<tcp::socket&>> ws_;
+    boost::asio::strand<boost::asio::io_context::executor_type> strand_;
+    boost::beast::multi_buffer buffer_;
+};
+
+//------------------------------------------------------------------------------
+
+// Accepts incoming connections and launches the server_network_sessions
+class listener : public std::enable_shared_from_this<listener> {
+public:
+    listener(boost::asio::io_context& ioc, ssl::context& ctx, tcp::endpoint endpoint);
+
+    // Start accepting incoming connections
+    void run();
+    void do_accept();
+    void on_accept(boost::system::error_code ec);
+
+private:
+    ssl::context& ctx_;
+    tcp::acceptor acceptor_;
+    tcp::socket socket_;
+};
+
+#endif /* end of include guard: SERVER_NETWORK_H */
