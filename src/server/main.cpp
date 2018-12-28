bfa03065ccb67c97ef627c28314613ec68e93874
John Agapeyev <jagapeyev@gmail.com>
2018-12-27 20:14:26 -0800

    Refactored cmake to support seperate client and server binaries and code bases

---
+++ src/server/main.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include "crypto.h"
+#include "session.h"
+
+void test() {
+    crypto::secure_array<int, 32> good({1, 4, 3, 5, 7, 8, 9, 2});
+    std::cout << good[3] << "\n";
+    good.fill(99);
+    std::cout << good[3] << "\n";
+}
+
+int main(void) {
+    test();
+    return EXIT_SUCCESS;
+}
