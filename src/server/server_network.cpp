836b023679e938a2eeebf30467ad012bf27a7d38
John Agapeyev <jagapeyev@gmail.com>
2019-01-08 14:53:36 -0800

    Moved handle request function into source file

--- src/server/server_network.cpp
+++ src/server/server_network.cpp
@@ -123,6 +123,102 @@ void http_session::do_close() {
     // At this point the connection is closed gracefully
 }
 
+// This function produces an HTTP response for the given
+// request. The type of the response object depends on the
+// contents of the request, so the interface requires the
+// caller to pass a generic lambda for receiving the response.
+template<class Body, class Allocator, class Send>
+void http_session::handle_request(
+        http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+    // Returns a bad request response
+    const auto bad_request = [&req](boost::beast::string_view why) {
+        http::response<http::string_body> res{http::status::bad_request, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = why.to_string();
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a not found response
+    const auto not_found = [&req](boost::beast::string_view target) {
+        http::response<http::string_body> res{http::status::not_found, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "The resource '" + target.to_string() + "' was not found.";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Returns a server error response
+    const auto server_error = [&req](boost::beast::string_view what) {
+        http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.keep_alive(req.keep_alive());
+        res.body() = "An error occurred: '" + what.to_string() + "'";
+        res.prepare_payload();
+        return res;
+    };
+
+    // Make sure we can handle the method
+    if (req.method() != http::verb::get && req.method() != http::verb::head) {
+        return send(bad_request("Unknown HTTP-method"));
+    }
+
+    // Request path must be absolute and not contain "..".
+    if (req.target().empty() || req.target()[0] != '/'
+            || req.target().find("..") != boost::beast::string_view::npos) {
+        return send(bad_request("Illegal request-target"));
+    }
+
+    // Build the path to the requested file
+    std::string path = req.target().to_string();
+    if (req.target().back() == '/') {
+        path.append("index.html");
+    }
+
+    // Attempt to open the file
+    boost::beast::error_code ec;
+    http::file_body::value_type body;
+    body.open(path.c_str(), boost::beast::file_mode::scan, ec);
+
+    // Handle the case where the file doesn't exist
+    if (ec == boost::system::errc::no_such_file_or_directory) {
+        return send(not_found(req.target()));
+    }
+
+    // Handle an unknown error
+    if (ec) {
+        return send(server_error(ec.message()));
+    }
+
+    // Cache the size since we need it after the move
+    const auto size = body.size();
+
+    // Respond to HEAD request
+    if (req.method() == http::verb::head) {
+        http::response<http::empty_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "text/html");
+        res.content_length(size);
+        res.keep_alive(req.keep_alive());
+        return send(std::move(res));
+    }
+
+    // Respond to GET request
+    http::response<http::file_body> res{std::piecewise_construct, std::make_tuple(std::move(body)),
+            std::make_tuple(http::status::ok, req.version())};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/html");
+    res.content_length(size);
+    res.keep_alive(req.keep_alive());
+
+    return send(std::move(res));
+}
+
 listener::listener(boost::asio::io_context& ioc, ssl::context& ssl_ctx, tcp::endpoint endpoint) :
         ctx(ssl_ctx), acceptor(ioc), socket(ioc) {
     boost::system::error_code ec;
