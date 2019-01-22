0d2d5455165830235ae8173f20e225d6954902d9
John Agapeyev <jagapeyev@gmail.com>
2019-01-21 21:07:03 -0800

    Added a few extra message serialization tests

--- src/test/message_serialize_test.cpp
+++ src/test/message_serialize_test.cpp
@@ -21,12 +21,58 @@ BOOST_AUTO_TEST_CASE(basic_serialization) {
     BOOST_TEST((message == deser));
 }
 
-BOOST_AUTO_TEST_CASE(basic_encode) {
+BOOST_AUTO_TEST_CASE(bad_serialization) {
     auto session = get_session();
     const auto message = session.ratchet_encrypt(get_message(), get_aad());
 
     auto mesg_str = serialize_message(message);
     mesg_str[0] = '8';
+
+    BOOST_REQUIRE_THROW(deserialize_message(mesg_str), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(empty_deserialization) {
+    const auto mesg_str = "";
+    BOOST_REQUIRE_THROW(deserialize_message(mesg_str), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(added_garbage_deserialization) {
+    auto session = get_session();
+    const auto message = session.ratchet_encrypt(get_message(), get_aad());
+
+    auto mesg_str = serialize_message(message);
+    mesg_str.push_back('a');
+    mesg_str.push_back(' ');
+    mesg_str.push_back('b');
+    mesg_str.push_back(' ');
+    mesg_str.push_back('c');
+    mesg_str.push_back(';');
+
+    const auto deser = deserialize_message(mesg_str);
+
+    BOOST_TEST((message == deser));
+}
+
+BOOST_AUTO_TEST_CASE(cutoff_deserialization) {
+    auto session = get_session();
+    const auto message = session.ratchet_encrypt(get_message(), get_aad());
+
+    auto mesg_str = serialize_message(message);
+    for (int i = 0; i < 10; ++i) {
+        mesg_str.pop_back();
+    }
+
+    BOOST_REQUIRE_THROW(deserialize_message(mesg_str), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(corrupted_deserialization) {
+    auto session = get_session();
+    const auto message = session.ratchet_encrypt(get_message(), get_aad());
+
+    auto mesg_str = serialize_message(message);
+    for (auto& c : mesg_str) {
+        c <<= 1;
+    }
 
     BOOST_REQUIRE_THROW(deserialize_message(mesg_str), std::runtime_error);
 }
