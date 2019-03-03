152c66fda754c6ef5f30320c5ef91d84ad3a46e5
John Agapeyev <jagapeyev@gmail.com>
2019-03-03 13:44:33 -0800

    Hooked up gui to existing functions

--- src/client/mainwindow.cpp
+++ src/client/mainwindow.cpp
@@ -20,6 +20,10 @@ void main_window::on_register_btn_clicked() {
     const auto email = ui->register_email->text().toStdString();
     const auto password = ui->register_password->text().toStdString();
     spdlog::info("Register button clicked for {} {}", email, password);
+
+    if (!dev.check_registration()) {
+        dev.register_with_server(email, password);
+    }
 }
 
 void main_window::on_verify_btn_clicked() {
@@ -28,6 +32,10 @@ void main_window::on_verify_btn_clicked() {
     const auto code = ui->register_code->text().toStdString();
 
     spdlog::info("Verify button clicked {} {} {}", email, password, code);
+
+    if (!dev.check_registration()) {
+        dev.confirm_registration(email, std::stoull(code));
+    }
 }
 
 void main_window::on_send_btn_clicked() {
@@ -35,8 +43,28 @@ void main_window::on_send_btn_clicked() {
     const auto contents = ui->msg_contents->toPlainText().toStdString();
 
     spdlog::info("Mesg Send button clicked {} {}", dest, contents);
+
+    crypto::secure_vector<std::byte> plain;
+    for (const auto c : contents) {
+        plain.push_back(std::byte{static_cast<unsigned char>(c)});
+    }
+
+    dev.send_signal_message(plain, crypto::secure_vector<std::string>{dest});
 }
 
 void main_window::on_recv_btn_clicked() {
     spdlog::info("Mesg Recv button clicked");
+
+    const auto messages = dev.receive_signal_message();
+
+    if (messages.has_value()) {
+        spdlog::info("Received {} messages", messages->size());
+        for (const auto& plain : *messages) {
+            std::stringstream ss;
+            for (const auto b : plain) {
+                ss << static_cast<unsigned char>(std::to_integer<unsigned char>(b));
+            }
+            spdlog::info("Received plaintext {}", ss.str());
+        }
+    }
 }
