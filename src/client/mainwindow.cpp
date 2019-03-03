e4aeb87e20c8d5ebd54134d316afca4e0f315985
John Agapeyev <jagapeyev@gmail.com>
2019-03-03 15:00:54 -0800

    Added error popup on exception

--- src/client/mainwindow.cpp
+++ src/client/mainwindow.cpp
@@ -1,4 +1,5 @@
 #include <QMainWindow>
+#include <QMessageBox>
 #include <QWidget>
 
 #include "logging.h"
@@ -19,10 +20,14 @@ main_window::~main_window() {
 void main_window::on_register_btn_clicked() {
     const auto email = ui->register_email->text().toStdString();
     const auto password = ui->register_password->text().toStdString();
-    spdlog::info("Register button clicked for {} {}", email, password);
+    spdlog::debug("Register button clicked for {} {}", email, password);
 
-    if (!dev.check_registration()) {
-        dev.register_with_server(email, password);
+    try {
+        if (!dev.check_registration()) {
+            dev.register_with_server(email, password);
+        }
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, tr("Something went wrong!"), tr(e.what()));
     }
 }
 
@@ -31,10 +36,14 @@ void main_window::on_verify_btn_clicked() {
     const auto password = ui->register_password->text().toStdString();
     const auto code = ui->register_code->text().toStdString();
 
-    spdlog::info("Verify button clicked {} {} {}", email, password, code);
+    spdlog::debug("Verify button clicked {} {} {}", email, password, code);
 
-    if (!dev.check_registration()) {
-        dev.confirm_registration(email, std::stoull(code));
+    try {
+        if (!dev.check_registration()) {
+            dev.confirm_registration(email, std::stoull(code));
+        }
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, tr("Something went wrong!"), tr(e.what()));
     }
 }
 
@@ -42,51 +51,61 @@ void main_window::on_send_btn_clicked() {
     const auto dest = ui->send_dest->text().toStdString();
     const auto contents = ui->msg_contents->toPlainText().toStdString();
 
-    spdlog::info("Mesg Send button clicked {} {}", dest, contents);
+    spdlog::debug("Mesg Send button clicked {} {}", dest, contents);
 
     crypto::secure_vector<std::byte> plain;
-    for (const auto c : contents) {
-        plain.push_back(std::byte{static_cast<unsigned char>(c)});
-    }
 
-    /*
-     * There's an issue where simultaneous initial messages creates 2 seperate sessions
-     * This prevents decryption on subsequent messages
-     * Now, technically, I could mess around with ensuring sessions are consistent, but
-     * currently it's easier to receive messages first, to ensure that cannot occur
-     *
-     * Fixing this would require message retry requests to discard the bad session on the sender side
-     */
-
-    const auto messages = dev.receive_signal_message();
-
-    if (messages.has_value()) {
-        spdlog::info("Received {} messages", messages->size());
-        for (const auto& plain : *messages) {
-            std::stringstream ss;
-            for (const auto b : plain) {
-                ss << static_cast<unsigned char>(std::to_integer<unsigned char>(b));
-            }
-            spdlog::info("Received plaintext {}", ss.str());
+    try {
+        for (const auto c : contents) {
+            plain.push_back(std::byte{static_cast<unsigned char>(c)});
         }
-    }
 
-    dev.send_signal_message(plain, crypto::secure_vector<std::string>{dest});
+        /*
+         * There's an issue where simultaneous initial messages creates 2 seperate sessions
+         * This prevents decryption on subsequent messages
+         * Now, technically, I could mess around with ensuring sessions are consistent, but
+         * currently it's easier to receive messages first, to ensure that cannot occur
+         *
+         * Fixing this would require message retry requests to discard the bad session on the sender side
+         */
+        const auto messages = dev.receive_signal_message();
+
+        if (messages.has_value()) {
+            spdlog::info("Received {} messages", messages->size());
+            for (const auto& plain : *messages) {
+                std::stringstream ss;
+                for (const auto b : plain) {
+                    ss << static_cast<unsigned char>(std::to_integer<unsigned char>(b));
+                }
+                spdlog::info("Received plaintext {}", ss.str());
+                QMessageBox::information(this, tr("SIGEMAIL"), tr(ss.str().c_str()));
+            }
+        }
+
+        dev.send_signal_message(plain, crypto::secure_vector<std::string>{dest});
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, tr("Something went wrong!"), tr(e.what()));
+    }
 }
 
 void main_window::on_recv_btn_clicked() {
-    spdlog::info("Mesg Recv button clicked");
+    spdlog::debug("Mesg Recv button clicked");
 
-    const auto messages = dev.receive_signal_message();
+    try {
+        const auto messages = dev.receive_signal_message();
 
-    if (messages.has_value()) {
-        spdlog::info("Received {} messages", messages->size());
-        for (const auto& plain : *messages) {
-            std::stringstream ss;
-            for (const auto b : plain) {
-                ss << static_cast<unsigned char>(std::to_integer<unsigned char>(b));
+        if (messages.has_value()) {
+            spdlog::info("Received {} messages", messages->size());
+            for (const auto& plain : *messages) {
+                std::stringstream ss;
+                for (const auto b : plain) {
+                    ss << static_cast<unsigned char>(std::to_integer<unsigned char>(b));
+                }
+                spdlog::info("Received plaintext {}", ss.str());
+                QMessageBox::information(this, tr("SIGEMAIL"), tr(ss.str().c_str()));
             }
-            spdlog::info("Received plaintext {}", ss.str());
         }
+    } catch (const std::exception& e) {
+        QMessageBox::critical(this, tr("Something went wrong!"), tr(e.what()));
     }
 }
