a7bffd3c948769960818a67a685dc76b21289c30
John Agapeyev <jagapeyev@gmail.com>
2019-03-27 22:01:56 -0700

    Added session logging for signal examples

--- src/test/session_test.cpp
+++ src/test/session_test.cpp
@@ -2,12 +2,12 @@
 #include <boost/test/unit_test.hpp>
 
 #include "crypto.h"
+#include "logging.h"
 #include "session.h"
 #include "test.h"
 
 BOOST_AUTO_TEST_SUITE(session_tests)
 
-#if 1
 BOOST_AUTO_TEST_CASE(session_initial) {
     const auto message = get_message();
     const auto aad = get_aad();
@@ -21,10 +21,22 @@ BOOST_AUTO_TEST_CASE(session_initial) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
-    auto [recv_s, plaintext] = decrypt_initial_message(
-            send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
+    spdlog::debug("Testing initial session message exchange");
+
+    const auto m = send_s.ratchet_encrypt(message, aad);
+
+    //auto [recv_s, plaintext] = decrypt_initial_message(send_s.ratchet_encrypt(message, aad), recv_id, recv_pre);
+    auto [recv_s, plaintext] = decrypt_initial_message(m, recv_id, recv_pre);
+
+    spdlog::info("Input plaintext   {}", message);
+    spdlog::info("Output ciphertext {}", m.message);
+    spdlog::info("Output plaintext  {}", plaintext);
+
+    spdlog::debug("Output plaintext == Input plaintext");
+    spdlog::debug("Test OK");
 
     BOOST_TEST(plaintext == message);
 }
@@ -42,7 +54,8 @@ BOOST_AUTO_TEST_CASE(session_double_send) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -63,7 +76,8 @@ BOOST_AUTO_TEST_CASE(session_initial_back_forth) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -87,7 +101,8 @@ BOOST_AUTO_TEST_CASE(session_double_decrypt) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -112,7 +127,8 @@ BOOST_AUTO_TEST_CASE(session_back_and_forth) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -134,19 +150,33 @@ BOOST_AUTO_TEST_CASE(out_of_order_messages) {
     crypto::DH_Keypair recv_id;
     crypto::DH_Keypair recv_pre;
 
+    spdlog::debug("Testing Out of Order message handling");
+
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
 
+    spdlog::info("Encrypt message 1");
     const auto m1 = send_s.ratchet_encrypt(message_1, aad_1);
+    spdlog::info("Done encrypting message 1");
+    spdlog::info("Encrypt message 2");
     const auto m2 = send_s.ratchet_encrypt(message_2, aad_2);
+    spdlog::info("Done encrypting message 2");
 
+    spdlog::info("Decrypt message 2");
     BOOST_TEST(recv_s.ratchet_decrypt(m2) == message_2);
+    spdlog::info("Decryption succeeded");
+    spdlog::info("Decrypt message 1");
     BOOST_TEST(recv_s.ratchet_decrypt(m1) == message_1);
+    spdlog::info("Decryption succeeded");
+
+    spdlog::debug("Both messages decrypted successfully");
+    spdlog::debug("Test OK");
 }
 
 BOOST_AUTO_TEST_CASE(out_of_order_back_and_forth) {
@@ -168,7 +198,8 @@ BOOST_AUTO_TEST_CASE(out_of_order_back_and_forth) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -191,7 +222,8 @@ BOOST_AUTO_TEST_CASE(many_sends) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -214,7 +246,8 @@ BOOST_AUTO_TEST_CASE(many_alternating) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
@@ -246,26 +279,46 @@ BOOST_AUTO_TEST_CASE(dh_old_message) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
 
+    spdlog::debug("Testing first Signal documentation example");
+
     //Send once
     //A1
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("A1 sent fine");
+    spdlog::info("A1 received fine");
     //Alternate once
     //B1
     BOOST_TEST(send_s.ratchet_decrypt(recv_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("B1 sent fine");
+    spdlog::info("B1 received fine");
 
     //Encrypt message on old DH values
     //B2 on old DH values
     const auto m = recv_s.ratchet_encrypt(message, aad);
     BOOST_TEST(send_s.ratchet_decrypt(m) == message);
 
+    spdlog::info("B2 sent fine");
+    spdlog::info("B2 received fine");
+
     //A3 & A4
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
+
+    spdlog::info("A3 sent fine");
+    spdlog::info("A3 received fine");
+    spdlog::info("A4 sent fine");
+    spdlog::info("A4 received fine");
+
+    spdlog::info("Decryption succeeded");
+
+    spdlog::debug("All message decrypted successfully");
+    spdlog::debug("Test OK");
 }
 
 //https://signal.org/docs/specifications/doubleratchet/#out-of-order-messages
@@ -283,41 +336,63 @@ BOOST_AUTO_TEST_CASE(signal_out_of_order_example) {
     auto key
             = crypto::X3DH_sender(send_id, send_ephem, recv_id.get_public(), recv_pre.get_public());
 
-    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt, std::vector<std::byte>{}};
+    session send_s{key, send_ephem, recv_pre.get_public(), send_id.get_public(), std::nullopt,
+            std::vector<std::byte>{}};
 
     auto [recv_s, plaintext] = decrypt_initial_message(
             send_s.ratchet_encrypt(get_message(), get_aad()), recv_id, recv_pre);
 
+    spdlog::debug("Testing final Signal documentation example");
+
     //Send once
     //A1
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("A1 sent fine");
+    spdlog::info("A1 received fine");
     //Alternate once
     //B1
     BOOST_TEST(send_s.ratchet_decrypt(recv_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("B1 sent fine");
+    spdlog::info("B1 received fine");
 
     //Encrypt message on old DH values
     //B2 on old DH values, not sent
     const auto m_1 = recv_s.ratchet_encrypt(message, aad);
 
+    spdlog::info("B2 sent fine");
+
     //A3 & A4
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("A3 sent fine");
+    spdlog::info("A3 received fine");
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("A4 sent fine");
+    spdlog::info("A4 received fine");
 
     //B3, not sent
     const auto m_2 = recv_s.ratchet_encrypt(message, aad);
+    spdlog::info("B3 sent fine");
 
     //B4
     BOOST_TEST(send_s.ratchet_decrypt(recv_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("B4 sent fine");
+    spdlog::info("B4 received fine");
 
     //A5
     BOOST_TEST(recv_s.ratchet_decrypt(send_s.ratchet_encrypt(message, aad)) == message);
+    spdlog::info("A5 sent fine");
+    spdlog::info("A5 received fine");
 
     //B3 out of order
     BOOST_TEST(send_s.ratchet_decrypt(m_2) == message);
+    spdlog::info("B3 received fine");
 
     //B2 out of order
     BOOST_TEST(send_s.ratchet_decrypt(m_1) == message);
+    spdlog::info("B2 received fine");
+
+    spdlog::debug("All messages decrypted successfully");
+    spdlog::debug("Test OK");
 }
-#endif
 
 BOOST_AUTO_TEST_SUITE_END()
